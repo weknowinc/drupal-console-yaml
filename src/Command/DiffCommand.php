154601f61b9370a9938f0db6a39e2a5688caca1f
Jose Angel Bonfil <joseangelbonfil@gmail.com>
2017-07-05 14:20:33 -0500

    adding translations files and also annotations to enable then on the … (#5)
    * adding translations files and also annotations to enable then on the yaml libraries
    
    * change on the extension library value
    
    * adding new line at the end of the unset yaml file

--- src/Command/DiffCommand.php
+++ src/Command/DiffCommand.php
@@ -16,7 +16,16 @@ use Symfony\Component\Console\Command\Command;
 use Drupal\Console\Core\Command\Shared\CommandTrait;
 use Drupal\Console\Core\Style\DrupalStyle;
 use Drupal\Console\Core\Utils\NestedArray;
+use Drupal\Console\Annotations\DrupalCommand;
 
+/**
+ * Class DiffCommand.
+ *
+ * @DrupalCommand (
+ *  extension="drupal/console-yaml",
+ *  extensionType="library"
+ * )
+ */
 class DiffCommand extends Command
 {
     use CommandTrait;
--- src/Command/MergeCommand.php
+++ src/Command/MergeCommand.php
@@ -15,7 +15,16 @@ use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Console\Command\Command;
 use Drupal\Console\Core\Command\Shared\CommandTrait;
 use Drupal\Console\Core\Style\DrupalStyle;
+use Drupal\Console\Annotations\DrupalCommand;
 
+/**
+ * Class MergeCommand.
+ *
+ * @DrupalCommand (
+ *     extension="drupal/console-yaml",
+ *     extensionType="library"
+ * )
+ */
 class MergeCommand extends Command
 {
     use CommandTrait;
